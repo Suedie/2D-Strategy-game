537dab0d163eed571ac4249e522c490c4ff8f644
Elias H. <98331725+Suedie@users.noreply.github.com>
2024-03-16 01:42:49 +0100

    Add TileMap class

---
+++ DeiGratia/src/TileMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeiGratia.src
+{
+    internal class TileMap
+    {
+        private List<TileSet> tileSets;
+        private List<TileMapLayer> tileMapLayers;
+
+        public TileMap(List<TileSet> tileSets, List<TileMapLayer> tileMapLayers)
+        {
+            this.tileSets = tileSets;
+            this.tileMapLayers = tileMapLayers;
+        }
+    }
+}
