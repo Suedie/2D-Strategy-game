d3542969fe8296058e326a20236aaeca15a4abb7
Suedie <elias.haqiqat@protonmail.com>
2025-01-16 02:00:33 +0100

    Add state for animations

--- DeiGratia/src/Entities/PlayerCharacter.cs
+++ DeiGratia/src/Entities/PlayerCharacter.cs
@@ -8,6 +8,7 @@ public class PlayerCharacter : IEntity
     private Vector2 _position;
     private int _healthPoints;
     private int _movementPoints;
+    private CharacterState _state;
     
     public int MovementPoints { get; set;}
 
@@ -16,6 +17,7 @@ public class PlayerCharacter : IEntity
     public PlayerCharacter(int x, int y)
     {
         Position = new Vector2(x, y);
+        _state = CharacterState.IDLE;
     }
     
     public void Move(Vector2 amount)
