bc7f936e666325747a5b4e8f5fa0e37bd080945c
Suedie <elias.haqiqat@protonmail.com>
2025-01-17 01:45:59 +0100

    Revert "Implement auto-properties"
    This reverts commit 07272d189982b3b89aa16b4cae986ae1f13a2d91.

--- DeiGratia/src/Tilemap/TMXLoader.cs
+++ DeiGratia/src/Tilemap/TMXLoader.cs
@@ -14,15 +14,15 @@ namespace DeiGratia.src.Tilemap
 
         }
 
-        public XmlDocument TmxFile { get; set;}
+        public XmlDocument TmxFile { get => _tmxFile; set => _tmxFile = value; }
 
         //Converts a .tmx map into a C# object
         public TileMap LoadMap(string filepath)
         {
-            _tmxFile.Load(filepath);
-            XmlNodeList layerNodes = _tmxFile.GetElementsByTagName("layer");
-            XmlNodeList tileSetNodes = _tmxFile.GetElementsByTagName("tileset");
-            XmlAttributeCollection mapAttributes = _tmxFile.GetElementsByTagName("map").Item(0).Attributes;
+            TmxFile.Load(filepath);
+            XmlNodeList layerNodes = TmxFile.GetElementsByTagName("layer");
+            XmlNodeList tileSetNodes = TmxFile.GetElementsByTagName("tileset");
+            XmlAttributeCollection mapAttributes = TmxFile.GetElementsByTagName("map").Item(0).Attributes;
 
             //Initialised with invalid-default values
             int width = -1;
