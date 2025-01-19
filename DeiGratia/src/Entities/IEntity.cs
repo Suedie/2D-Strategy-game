01c36fb9c32e54cd3ddc5935ce5eeade07439667
Suedie <elias.haqiqat@protonmail.com>
2025-01-19 17:31:24 +0100

    Generalize rendering

--- DeiGratia/src/Entities/IEntity.cs
+++ DeiGratia/src/Entities/IEntity.cs
@@ -1,9 +1,11 @@
+using System;
 using Microsoft.Xna.Framework;
 
 namespace DeiGratia.src.Entities;
 
 public interface IEntity
 { 
+    String Name { get; set; }
     Vector2 Position { get; set; }
     
     void Move(Vector2 delta);
