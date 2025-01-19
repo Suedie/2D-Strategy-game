028d60ebbf04ae74a2138c85147f4fc2d1da6d5b
Suedie <elias.haqiqat@protonmail.com>
2025-01-19 16:03:42 +0100

    Add player idle animation

--- DeiGratia/src/Graphics/EntityRenderer.cs
+++ DeiGratia/src/Graphics/EntityRenderer.cs
@@ -13,6 +13,11 @@ public class EntityRenderer
     private SpriteBatch _spriteBatch;
     private ContentManager _content;
     
+    private float _frametime = 200f;
+    private float _timer;
+    private int _currentFrame = 0;
+    private int _totalFrames = 8;
+    
     public EntityRenderer(IEntity player, SpriteBatch spriteBatch, ContentManager content)
     {
         this._player = player;
@@ -26,11 +31,19 @@ public class EntityRenderer
         _content = content;
     }
 
-    public void DrawPlayer()
+    public void DrawPlayer(GameTime gameTime)
     {
         _texture = _content.Load<Texture2D>("MiniCavalierMan");
+        _timer += gameTime.ElapsedGameTime.Milliseconds;
+
+        if (_timer >= _frametime)
+        {
+            _timer -= _frametime;
+            _currentFrame = (_currentFrame + 1) % _totalFrames;
+        }
+        
         _spriteBatch.Draw(_texture, _player.Position,
-            new Rectangle(0, 0, 32, 32), Color.White, 0f, Vector2.Zero,
+            new Rectangle(_currentFrame * 32, 0, 32, 32), Color.White, 0f, Vector2.Zero, 
             1f, SpriteEffects.None, 0.3f);
     }
     
