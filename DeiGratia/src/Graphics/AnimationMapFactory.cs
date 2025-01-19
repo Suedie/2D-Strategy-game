45e959040e7ede26caf06dec2ec57e9d8e0bfa45
Suedie <elias.haqiqat@protonmail.com>
2025-01-19 19:49:05 +0100

    Add AnimationMapFactory.cs

---
+++ DeiGratia/src/Graphics/AnimationMapFactory.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using DeiGratia.src.Entities;
+
+namespace DeiGratia.Graphics;
+
+public class AnimationMapFactory
+{
+    Dictionary<CharacterState, int> frameMap = new Dictionary<CharacterState, int>();
+    
+}
