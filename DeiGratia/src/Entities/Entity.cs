7f3c749815117a3e6f22bda428bd88dcd3ed98db
Suedie <elias.haqiqat@protonmail.com>
2025-01-19 17:20:13 +0100

    Refactor PlayerCharacter.cs to Cavalier

---
+++ DeiGratia/src/Entities/Entity.cs
@@ -0,0 +1,15 @@
+using Microsoft.Xna.Framework;
+
+namespace DeiGratia.src.Entities;
+
+public abstract class Entity : IEntity
+{
+    public Vector2 Position { get; set; }
+    private int _healthPoints;
+    private int _movementPoints;
+    public CharacterState State { get; set; }
+    public void Move(Vector2 amount)
+    {
+        Position += amount;
+    }
+}
